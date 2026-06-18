bed83455c66037b831dc097684440340be2b3983
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-18 23:53:08 +0500

    Create default.nix

---
+++ home-manager/modules/waybar/default.nix
@@ -0,0 +1,146 @@
+{ config, pkgs, ... }: 
+
+{
+    programs.waybar = {
+        enable = true;
+        style = ./style.css;
+        settings = {
+            mainBar = {
+                layer = "top";
+                position = "top";
+                height = 30;
+                
+                modules-left = [
+                    "custom/logo"
+                    "hyprland/workspaces"
+                ];
+
+                modules-center = [
+                    "backlight"
+                    "clock"
+                ];
+
+                modules-right = [
+                    "tray"
+                    "memory"
+                    "cpu"
+                    "network"
+                    "wireplumber"
+                    "battery"
+                ];
+
+                "wlr/taskbar" = {
+                    format = "{icon}";
+                    on-click = "activate";
+                    on-click-right = "fullscreen";
+                    icon-theme = "WhiteSur";
+                    icon-size = 25;
+                    tooltip-format = "{title}";
+                };
+
+                "hyprland/workspaces" = {
+                    on-click = "activate";
+                    format = "{icon}"; 
+                    format-icons = {
+                        default = "";
+                        "1" = "";
+                        "2" = "󰈹";
+                        "3" = "";
+                        "4" = "";
+                        "5" = "󱧶";
+                        "6" = "󰠮";
+                        "active" = "󱓻";
+                        "urgent" = "󱓻";
+                    }; 
+
+                    "persistent-workspaces" = {
+                        "1" = [];
+                        "2" = [];
+                        "3" = [];
+                        "4" = [];
+                        "5" = [];
+                        "6" = [];
+                    };
+                };
+
+                "backlight" = {
+                    interval = 2;
+                    format = "  {percent}%";
+                };
+
+                "memory" = {
+                    interval = 5;
+                    format = "  {}%";
+                    max-length = 10;
+                };
+
+                "cpu" = {
+                    interval=  1;
+                    format = "  {usage}%";
+                    max-length = 10;
+                };
+
+                "tray" = {
+                    spacing = 11;
+                };
+
+                "clock" = {
+                    tooltip-format = "{calendar}";
+                    format-alt = "  {:%a, %d %b %Y}";
+                    format = "  {:%I:%M %p}";
+                };
+
+                "network" = {
+                    format-wifi = "{icon}";
+                    format-icons = ["󰤯" "󰤟" "󰤢" "󰤥" "󰤨"];
+                    format-ethernet = "󰀂";
+	                format-alt = "󱛇";
+                    format-disconnected = "󰖪";
+	                tooltip-format-wifi = "{icon} {essid}\n⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
+                    tooltip-format-ethernet = "󰀂  {ifname}\n⇣{bandwidthDownBytes}  ⇡{bandwidthUpBytes}";
+	                tooltip-format-disconnected = "Disconnected";
+	                interval = 5;
+	                nospacing = 1;
+                };
+
+                "wireplumber" = {
+                    format = "{icon}  {volume}%";
+                    format-bluetooth = "󰂰";
+                    nospacing = 1;
+                    tooltip-format = "Volume : {volume}%";
+                    format-muted = "󰝟";
+                    format-headphone = "";
+                    format-icons = {
+                        headphone = "";
+                        default = ["󰖀" "󰕾" ""];
+                    };
+                    on-click = "pamixer -t";
+                    scroll-step = 1;
+                };
+
+                "custom/logo" = {
+                    format = "  NixOS ";
+                    tooltip = false;
+                    on-click = "wofi --show drun";
+                };
+
+                "battery" = {
+                    format = "{capacity}% {icon}";
+                    format-icons = {
+                        charging = [ "󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅" ];
+                        default = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+                    };
+                    
+                    format-full = "Charged ";
+                    interval = 5;
+                    states = {
+                        warning = 20;
+                        critical = 10;
+                    };
+
+                    tooltip = false;
+                };
+            };
+        };
+    };
+}
