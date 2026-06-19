ecb8822c82347422375a482b6d429263aa5dcad6
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-19 15:26:15 +0500

    Update home-pkgs.nix

--- home-manager/home-pkgs.nix
+++ home-manager/home-pkgs.nix
@@ -4,21 +4,37 @@
     nixpkgs.config.allowUnfree = true;
     home.packages = with pkgs; [
         waybar
+        swaynotificationcenter
 
         # GUI apps
         obs-studio
-        
+        telegram-desktop
+        zed-editor
+        pavucontrol
+
         # CLI and TUI
         fzf
         htop
+        btop
+        wofi
         microfetch
         onefetch
+        fastfetch
+        nitch
         grim
         slurp
         swaybg
         hyprlock
+        hyprpicker
         peaclock
-        
+        dpkg
+        yazi
+        eza
+        bat
+        tree
+        zoxide
+        pfetch 
+
         libnotify
         xdg-desktop-portal-gtk
         xdg-desktop-portal-hyprland
