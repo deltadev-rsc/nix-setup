f43466a78047fd0f4446d789fd85dc55c9afdc3f
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-23 23:22:53 +0500

    Create fastfetch.nix

---
+++ home-manager/modules/utils/fastfetch.nix
@@ -0,0 +1,126 @@
+{ pkgs, ... }:
+
+{
+    programs.fastfetch = {
+        enable = true;
+        settings = {
+            logo = {
+                type = "auto";
+                padding = {
+                    top = 2;
+                };
+            };
+
+            display = {
+                separator = "   ";
+            };
+
+            modules = [
+                "title"
+                "separator"
+                {
+                    type = "os";
+                    key = " OS";
+                    keyColor = "yellow";
+                    format = 2;
+                }
+                {
+                    type = "os";
+                    key = "├";
+                    keyColor = "yellow";
+                }
+                {
+                    type = "kernel";
+                    key = "├";
+                    keyColor = "yellow";
+                }
+                {
+                    type = "packages";
+                    key = "├󰏖";
+                    keyColor = "yellow";
+                }
+                {
+                    type = "shell";
+                    key = "└";
+                    keyColor = "yellow";
+                }
+                "break"
+
+                {
+                    type = "wm";
+                    key = " DE/WM";
+                    keyColor = "blue";
+                }
+                {
+                    type = "lm";
+                    key = "├󰧨";
+                    keyColor = "blue";
+                }
+                {
+                    type = "wmtheme";
+                    key = "├󰉼";
+                    keyColor = "blue";
+                }
+                {
+                    type = "icons";
+                    key = "├󰀻";
+                    keyColor = "blue";
+                }
+                {
+                    type = "terminal";
+                    key = "├";
+                    keyColor = "blue";
+                }
+                {
+                    type = "wallpaper";
+                    key = "└󰸉";
+                    keyColor = "blue";
+                }
+
+                "break"
+                {
+                    type = "host";
+                    key = "󰌢 PC";
+                    keyColor = "green";
+                }
+                {
+                    type = "cpu";
+                    key = "├󰻠";
+                    keyColor = "green";
+                }
+                {
+                    type = "gpu";
+                    key = "├󰍛";
+                    keyColor = "green";
+                }
+                {
+                    type = "disk";
+                    key = "├";
+                    keyColor = "green";
+                }
+                {
+                    type = "memory";
+                    key = "├󰑭";
+                    keyColor = "green";
+                }
+                {
+                    type = "swap";
+                    key = "├󰓡";
+                    keyColor = "green";
+                }
+                {
+                    type = "display";
+                    key = "├󰍹";
+                    keyColor = "green";
+                }
+                {
+                    type = "uptime";
+                    key = "└󰅐";
+                    keyColor = "green";
+                }
+                "break"
+                "colors"
+            ];
+        };
+    };
+}
