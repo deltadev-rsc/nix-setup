91827f6b6f19b3ec15975c45369604a0a5d71079
Deltaqxq <deltaqxq174@mail.ru>
2026-08-27 17:10:12 +0000

    Update default.nix

--- home-manager/modules/kitty/default.nix
+++ home-manager/modules/kitty/default.nix
@@ -1,68 +1,21 @@
-{ config, pkgs, ... }: 
+{ config, pkgs, ... }:
 
 {
+    imports = [
+        # ./catp.nix # for catppuccin
+        ./tokyonight.nix # for tokyonight
+        # ./ayu.nix # for ayu
+    ];
+
     programs.kitty = {
         enable = true;
     };
-    
+
     programs.kitty.settings = {
         font_family = "JetBrainsMono Nerd Font";
         cursor_trail = 3;
         offset_x = 0;
         offset_y = 0;
-
-        foreground = "#cdd6f4";
-        background = "#1e1e2e";
-        selection_foreground = "#1e1e2e";
-        selection_background = "#f5e0dc";
-
-        cursor = "#f5e0dc";
-        cursor_text_color = "#1e1e2e";
-        scrollbar_handle_color = "#9399b2";
-        scrollbar_track_color = "#45475a";
-        url_color = "#f5e0dc";
-        active_border_color = "#b4befe";
-        inactive_border_color = "#6c7086";
-        bell_border_color = "#f9e2af";
-
-        wayland_titlebar_color = "system";
-        macos_titlebar_color = "system";
-
-        active_tab_foreground = "#11111b";
-        active_tab_background = "#cba6f7";
-        inactive_tab_foreground = "#cdd6f4";
-        inactive_tab_background = "#181825";
-        tab_bar_background = "#11111b";
-
-        mark1_foreground = "#1e1e2e";
-        mark1_background = "#b4befe";
-        mark2_foreground = "#1e1e2e";
-        mark2_background = "#cba6f7";
-        mark3_foreground = "#1e1e2e";
-        mark3_background = "#74c7ec";
-
-        color0 = "#1e1e2e";
-        color8 = "#313244";
-
-        color1 = "#f38ba8";
-        color9 = "#f38ba8";
-
-        color2  = "#a6e3a1";
-        color10 = "#a6e3a1";
-
-        color3  = "#f9e2af";
-        color11 = "#f9e2af";
-
-        color4  = "#89b4fa";
-        color12 = "#89b4fa";
-
-        color5  = "#f5c2e7";
-        color13 = "#f5c2e7";
-
-        color6  = "#94e2d5";
-        color14 = "#94e2d5";
-
-        color7  = "#bac2de";
-        color15 = "#cdd6f4";
+        #        background_opacity = 0.85;
     };
 }
