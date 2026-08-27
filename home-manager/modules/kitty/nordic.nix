4ca637fa008a480c009ea53bc157a4fd15847941
Deltaqxq <deltaqxq174@mail.ru>
2026-08-27 17:10:44 +0000

    Create nordic.nix

---
+++ home-manager/modules/kitty/nordic.nix
@@ -0,0 +1,57 @@
+{
+    programs.kitty.settings = {
+        foreground = "#d8dee9";
+        background = "#2e3440";
+        selection_foreground = "#eceff4";
+        selection_background = "#4c566a";
+
+        cursor = "#d8dee9";
+        cursor_text_color = "#2e3440";
+        scrollbar_handle_color = "#4c566a";
+        scrollbar_track_color = "#2e3440";
+        url_color = "#88c0d0";
+        active_border_color = "#88c0d0";
+        inactive_border_color = "#4c566a";
+        bell_border_color = "#ebcb8b";
+
+        wayland_titlebar_color = "system";
+        macos_titlebar_color = "system";
+
+        active_tab_foreground = "#2e3440";
+        active_tab_background = "#88c0d0";
+        inactive_tab_foreground = "#d8dee9";
+        inactive_tab_background = "#3b4252";
+        tab_bar_background = "#2e3440";
+
+        mark1_foreground = "#2e3440";
+        mark1_background = "#88c0d0";
+        mark2_foreground = "#2e3440";
+        mark2_background = "#b48ead";
+        mark3_foreground = "#2e3440";
+        mark3_background = "#8fbcbb";
+
+        color0 = "#3b4252";
+        color8 = "#4c566a";
+
+        color1 = "#bf616a";
+        color9 = "#bf616a";
+
+        color2 = "#a3be8c";
+        color10 = "#a3be8c";
+
+        color3 = "#ebcb8b";
+        color11 = "#ebcb8b";
+
+        color4 = "#81a1c1";
+        color12 = "#81a1c1";
+
+        color5 = "#b48ead";
+        color13 = "#b48ead";
+
+        color6 = "#88c0d0";
+        color14 = "#88c0d0";
+
+        color7 = "#e5e9f0";
+        color15 = "#eceff4";
+    };
+}
