293798a7f79a371951d3a994c1f92cca48d8b009
Deltaqxq <deltaqxq174@mail.ru>
2026-08-27 17:11:10 +0000

    Create ayu.nix

---
+++ home-manager/modules/kitty/ayu.nix
@@ -0,0 +1,58 @@
+{
+    programs.kitty.settings = {
+        foreground = "#dacbe4";
+        background = "#1f2430";
+        selection_foreground = "#dacbe4";
+        selection_background = "#4a5075";
+
+        cursor = "#dacbe4";
+        cursor_text_color = "#24283b";
+        scrollbar_handle_color = "#4a5075";
+        scrollbar_track_color = "#24283b";
+        url_color = "#73c6b6";
+        active_border_color = "#73c6b6";
+        inactive_border_color = "#4a5075";
+        bell_border_color = "#cda45e";
+
+        wayland_titlebar_color = "system";
+        macos_titlebar_color = "system";
+
+        active_tab_foreground = "#24283b";
+        active_tab_background = "#73c6b6";
+        inactive_tab_foreground = "#939ba3";
+        inactive_tab_background = "#2e334d";
+        tab_bar_background = "#24283b";
+
+        mark1_foreground = "#24283b";
+        mark1_background = "#73c6b6";
+        mark2_foreground = "#24283b";
+        mark2_background = "#c3a6aa";
+        mark3_foreground = "#24283b";
+        mark3_background = "#81a2be";
+
+        color0 = "#24283b";
+        color8 = "#535c7d";
+
+        color1 = "#f28779";
+        color9 = "#f28779";
+
+        color2 = "#d2fb7f";
+        color10 = "#d2fb7f";
+
+        color3 = "#fbc965";
+        color11 = "#fbc965";
+
+        color4 = "#5bcce2";
+        color12 = "#5bcce2";
+
+        color5 = "#dcbcfb";
+        color13 = "#dcbcfb";
+
+        color6 = "#93e2c8";
+        color14 = "#93e2c8";
+
+        color7 = "#939ba3";
+        color15 = "#dacbe4";
+    };
+}
+
